1b5bfd264ca6e999b7f905a11a5de36d2e0f11a6
Grzesiek Kolodziejczyk <gkolodziejczyk@gmail.com>
2014-03-27 18:06:37 +0100

    Remove not working safari workaround

--- lib/bookingsync/engine/session_helpers.rb
+++ lib/bookingsync/engine/session_helpers.rb
@@ -1,11 +1,5 @@
-require 'useragent'
-
 module BookingSync::Engine::SessionHelpers
   extend ActiveSupport::Concern
-
-  included do
-    before_filter :safari_iframe_cookie_fix, if: -> { BookingSync::Engine.embedded }
-  end
 
   private
 
@@ -19,25 +13,6 @@ module BookingSync::Engine::SessionHelpers
 
     if last_visit && (Time.now.to_i - last_visit > BookingSync::Engine.sign_out_after)
       clear_authorization!
-    end
-  end
-
-  # Safari ignores cookies in iframes until the user clicks within them.
-  #
-  # This fix shows a button when the user first opens the app in an iframe,
-  # which tells safari to load the app's cookies. Then the normal OAuth
-  # process can continue.
-  def safari_iframe_cookie_fix
-    if UserAgent.parse(request.user_agent).browser == "Safari"
-      return if session[:safari_iframe_cookie_fixed]
-
-      if params[:safari_iframe_cookie_fix].present?
-        session[:safari_iframe_cookie_fixed] = true
-      else
-        allow_bookingsync_iframe
-        render "sessions/safari_iframe_cookie_fix", layout: "application",
-          locals: {bookingsync_account_id: params[:_bookingsync_account_id]}
-      end
     end
   end
 end
