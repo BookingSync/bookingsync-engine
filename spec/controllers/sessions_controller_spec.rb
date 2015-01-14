cbe6a241de2d0d80894db26e19cb7db909b58de9
Piotr Marciniak <mandaryyyn@gmail.com>
2015-01-14 15:17:46 +0100

    fixes #28, routes should be included to apps via mount in routes.rb
    mount BookingSync::Engine => '/'

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe SessionsController, type: :controller do
 
     it "loads or creates account from omniauth auth" do
       expect(Account).to receive(:from_omniauth).and_call_original
-      get :create, provider: :bookingsync
+      get :create, provider: :bookingsync, use_route: 'bookingsync'
     end
 
     it "runs the account_authorized callback" do
       expect(controller).to receive(:account_authorized)
-      get :create, provider: :bookingsync
+      get :create, provider: :bookingsync, use_route: 'bookingsync'
     end
 
     it "redirects to after_bookingsync_sign_in_path" do
       expect(controller).to receive(:after_bookingsync_sign_in_path).and_return("/admin")
-      get :create, provider: :bookingsync
+      get :create, provider: :bookingsync, use_route: 'bookingsync'
       expect(response).to redirect_to("/admin")
     end
   end
@@ -26,12 +26,12 @@ RSpec.describe SessionsController, type: :controller do
   describe "GET destroy" do
     it "clears authorization" do
       expect(controller).to receive(:clear_authorization!)
-      get :destroy
+      get :destroy, use_route: 'bookingsync'
     end
 
     it "redirects to after_bookingsync_sign_out_path" do
       expect(controller).to receive(:after_bookingsync_sign_out_path).and_return("/signed_out")
-      get :destroy
+      get :destroy, use_route: 'bookingsync'
       expect(response).to redirect_to("/signed_out")
     end
   end
@@ -40,7 +40,7 @@ RSpec.describe SessionsController, type: :controller do
     context "when Engine is embedded" do
       before { BookingSync::Engine.embedded! }
       it "clears X-Frame-Options" do
-        get :failure
+        get :failure, use_route: 'bookingsync'
         expect(response.headers["X-Frame-Options"]).to eql("")
       end
     end
@@ -49,7 +49,7 @@ RSpec.describe SessionsController, type: :controller do
       before { BookingSync::Engine.standalone! }
 
       it "leaves X-Frame-Options without change" do
-        get :failure
+        get :failure, use_route: 'bookingsync'
         expect(response.headers["X-Frame-Options"]).to eql("SAMEORIGIN")
       end
     end
