aaeb32929f6c7b6126e1b8e3c12db2c42c1726e3
Mariusz Pietrzyk <wijet@wijet.pl>
2015-06-10 12:37:50 +0200

    Add retry on timeout

--- lib/bookingsync/engine/api_client.rb
+++ lib/bookingsync/engine/api_client.rb
@@ -6,13 +6,27 @@ class BookingSync::Engine::APIClient < BookingSync::API::Client
 
   def call(method, path, data = nil, options = nil)
     tries ||= 1
-    super
+    retry_on_timeout { super }
   rescue BookingSync::API::Unauthorized => e
     token_expired = e.headers["www-authenticate"].include?("The access token expired")
     if token_expired && (tries -= 1) >= 0
-      @token = @account.refresh_token!.token
+      retry_on_timeout { @token = @account.refresh_token!.token }
       retry
     else
+      raise
+    end
+  end
+
+  # Temporary fix to investigate broken oauth tokens
+  def retry_on_timeout
+    tries ||= 0
+    yield
+  rescue Faraday::TimeoutError => e
+    if (tries += 1) <= 3
+      Rails.logger.error("[bookingsync-engine] Retry #{tries} Rescued #{e} for account: #{@account.id}")
+      retry
+    else
+      Rails.logger.error("[bookingsync-engine] After #{tries} re-reising #{e} for account: #{@account.id}")
       raise
     end
   end
