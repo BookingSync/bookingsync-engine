c3aacad83e2c89dec98136e2a2a58faca8f66373
Mariusz Pietrzyk <wijet@wijet.pl>
2015-06-10 15:59:53 +0200

    Don't repeat retry, this one is in the client app

--- lib/bookingsync/engine/api_client.rb
+++ lib/bookingsync/engine/api_client.rb
@@ -6,7 +6,7 @@ class BookingSync::Engine::APIClient < BookingSync::API::Client
 
   def call(method, path, data = nil, options = nil)
     tries ||= 1
-    retry_on_timeout { super }
+    super
   rescue BookingSync::API::Unauthorized => e
     token_expired = e.headers["www-authenticate"].include?("The access token expired")
     if token_expired && (tries -= 1) >= 0
